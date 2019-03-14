94b012f93cdedf663c683249434b2c84af405336
Jannik Petersen <glutexo@live.de>
2019-03-14 15:15:12 +0100

    Added TimeManager class template

--- Poprica/Poprica/Scripts/Progress.cs
+++ Poprica/Poprica/Scripts/Progress.cs
@@ -30,9 +30,15 @@ namespace Poprica
             return false;
         }
 
+        #region Main
+        public static DateTime DateTime;
+        public static Dictionary<DialogueEntityName, Waifu> waifus;
+        #endregion
+
+        #region DC
         public static Vector3 DungeonPos;
         public static Vector3 DungeonRot;
+        #endregion
 
-        public static Dictionary<DialogueEntityName, Waifu> waifus;
     }
 }
---
+++ Poprica/Poprica/Scripts/TimeManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poprica
+{
+    public class TimeManager
+    {
+
+        #region Singleton
+
+        private static TimeManager main;
+
+        public static TimeManager Main
+        {
+            get
+            {
+                if (main == null)
+                {
+                    main = new TimeManager();
+                }
+                return main;
+            }
+        }
+
+        #endregion
+
+        public DateTime DateTime { get; set; }
+
+        private TimeManager()
+        {
+            DateTime = Progress.DateTime;
+        }
+
+        public DateTime CheckTime()
+        {
+            return DateTime;
+        }
+
+        public void SetTime(DateTime newDateTime)
+        {
+            DateTime = newDateTime;
+        }
+
+        public void IncreaseTime(int minutes)
+        {
+            DateTime.AddMinutes(minutes);
+        }
+
+        public void IncreaseDay(int days)
+        {
+            DateTime.AddDays(days);
+        }
+
+
+
+    }
+}
